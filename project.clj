64ced64ffe8da5f098b968796704de4f5c8844ff
Milt Reder <milt@anestuary.com>
2016-07-26 11:29:38 -0400

    release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.yetanalytics/ring-etag-middleware "0.1.0-SNAPSHOT"
+(defproject com.yetanalytics/ring-etag-middleware "0.1.1"
   :description "A bold new etag middleware"
   :url "https://github.com/yetanalytics/ring-etag-middleware"
   :license {:name "Eclipse Public License"
