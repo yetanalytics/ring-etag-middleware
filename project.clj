6cf26449b79be07b8c0d02caecb0ed925d6d7237
Milt Reder <milt@anestuary.com>
2016-07-26 11:29:21 -0400

    updated pandect

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [pandect "0.5.4"]]
+                 [pandect "0.6.0"]]
 
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]
